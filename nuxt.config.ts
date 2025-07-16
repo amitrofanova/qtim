40ad86e81c3df4f9d8b32ab70d8edd2cda83f31e
alena <mittralena@yandex.ru>
2025-07-16 10:20:23 +0500

    add colors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,9 @@ export default defineNuxtConfig({
   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
   ssr: false,
+  ui: {
+    theme: {
+      colors: ["primary", "secondary", "accent"],
+    },
+  },
 });
